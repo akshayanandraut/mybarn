55786ca78b559940c5f0fef5d368e34261a06464
Akshayanand Raut <akshay.raut0303@gmail.com>
2018-11-03 23:54:43 +0530

    Added node main.js

--- main.js
+++ main.js
@@ -1,23 +1,10 @@
-var express = require('express');
-var app = express();
+const express = require('express')
+const path = require('path')
+const PORT = process.env.PORT || 8080
 
-// set the port of our application
-// process.env.PORT lets the port be set by Heroku
-var port = process.env.PORT || 8080;
-
-// set the view engine to ejs
-app.set('view engine', 'ejs');
-
-// make express look in the public directory for assets (css/js/img)
-app.use(express.static(__dirname + '/public'));
-
-// set the home page route
-app.get('/', function (req, res) {
-
-    // ejs render automatically looks in the views folder
-    res.render('index');
-});
-
-app.listen(port, function () {
-    console.log('Our app is running on http://localhost:' + port);
-});
+express()
+    .use(express.static(path.join(__dirname, 'public')))
+    .set('views', path.join(__dirname, 'views'))
+    .set('view engine', 'ejs')
+    .get('/', (req, res) => res.render('index'))
+    .listen(PORT, () => console.log(`Listening on ${PORT}`))
