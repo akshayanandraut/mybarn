648ea3b48481da3c6b73c46c55a81b6a604672db
akshayanandraut <akshay.raut0303@gmail.com>
2018-11-04 20:40:58 +0530

    Added partial pages and breeding and cattle json db data structure

--- main.js
+++ main.js
@@ -1,10 +1,20 @@
-const express = require('express')
+const express = require('express');
 const path = require('path')
 const PORT = process.env.PORT || 8080
+var app = express();
 
-express()
+app
     .use(express.static(path.join(__dirname, 'public')))
     .set('views', path.join(__dirname, 'views'))
     .set('view engine', 'ejs')
-    .get('/', (req, res) => res.render('index'))
+    .get('/', (req, res) => res.render('index', {
+        page: "index"
+    }))
+    .get('/:page', function (req, res) {
+        var page = req.params.page;
+        console.log(page);
+        res.render("index", {
+            page: page
+        })
+    })
     .listen(PORT, () => console.log(`Listening on ${PORT}`))
